6a76e41aac7b80fd61185420dbece29f7f96b4a2
PatrickBastosDeveloper <patrickbastosc@gmail.com>
2025-02-22 15:53:24 -0300

    feat: Implement responsive Navbar with toggle menu for mobile view

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,10 +1,16 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 const Navbar = () => {
+  const [isOpen, setIsOpen] = useState(false);
+
+  const toggleMenu = () => {
+    setIsOpen(!isOpen);
+  };
+
   return (
     <nav className='text-white px-8 md:px-16 lg:px-24 border border-white rounded-lg'>
-        <div className='container py-2 flex justify-center md:justify-between items-center'>
-            <div className='space-x-6'>
+        <div className='container py-2 flex justify-between items-center'>
+            <div className='space-x-6 hidden md:flex'>
                 <a href="#home" className='hover:text-gray-400'>Home</a>
                 <a href="#about" className='hover:text-gray-400'>About Me</a>
                 <a href="#service" className='hover:text-gray-400'>Services</a>
@@ -13,7 +19,23 @@ const Navbar = () => {
             </div>
             <button className='bg-gradient-to-r from-green-400 to-blue-500 text-white hidden md:inline
             transform transition-transform duration-300 hover:scale-105 px-4 py-2 rounded-full'>Connect Me</button>
+            <div className='md:hidden'>
+                <button onClick={toggleMenu} className='focus:outline-none'>
+                    <svg className='w-6 h-6' fill='none' stroke='currentColor' viewBox='0 0 24 24' xmlns='http://www.w3.org/2000/svg'>
+                        <path strokeLinecap='round' strokeLinejoin='round' strokeWidth='2' d='M4 6h16M4 12h16m-7 6h7'></path>
+                    </svg>
+                </button>
+            </div>
         </div>
+        {isOpen && (
+            <div className='md:hidden'>
+                <a href="#home" className='block py-2 hover:text-gray-400'>Home</a>
+                <a href="#about" className='block py-2 hover:text-gray-400'>About Me</a>
+                <a href="#service" className='block py-2 hover:text-gray-400'>Services</a>
+                <a href="#project" className='block py-2 hover:text-gray-400'>Projects</a>
+                <a href="#contact" className='block py-2 hover:text-gray-400'>Contact</a>
+            </div>
+        )}
     </nav>
   )
 }

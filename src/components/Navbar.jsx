ef44ab20c3e7591de0f8a8811847b9ef372335ad
PatrickBastosDeveloper <patrickbastosc@gmail.com>
2025-02-22 17:21:01 -0300

    feat: Enhance mobile Navbar toggle button with border and padding

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -20,7 +20,7 @@ const Navbar = () => {
             <button className='bg-gradient-to-r from-green-400 to-blue-500 text-white hidden md:inline
             transform transition-transform duration-300 hover:scale-105 px-4 py-2 rounded-full'>Connect Me</button>
             <div className='md:hidden'>
-                <button onClick={toggleMenu} className='focus:outline-none'>
+                <button onClick={toggleMenu} className='focus:outline-none border border-white p-1 rounded'>
                     <svg className='w-6 h-6' fill='none' stroke='currentColor' viewBox='0 0 24 24' xmlns='http://www.w3.org/2000/svg'>
                         <path strokeLinecap='round' strokeLinejoin='round' strokeWidth='2' d='M4 6h16M4 12h16m-7 6h7'></path>
                     </svg>

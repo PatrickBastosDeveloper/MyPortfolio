0954255a4016067d72399dc60f7c03177386c996
PatrickBastosDeveloper <patrickbastosc@gmail.com>
2025-02-22 17:25:30 -0300

    feat: Add border to About component image for improved visibility

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -10,7 +10,7 @@ const About = () => {
           <img
             src={AboutImage}
             alt=""
-            className="w-72 h-80 rounded object-cover mb-8 md:mb-0"
+            className="w-72 h-80 rounded object-cover mb-8 md:mb-0 border-4 border-white"
           />
           <div className="flex-1">
             <p className="text-lg mb-8">

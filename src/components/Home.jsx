6bb1d6ab695c89e8f0b1d4d638849c304b52f7d8
PatrickBastosDeveloper <patrickbastosc@gmail.com>
2025-02-22 15:53:17 -0300

    feat: Remove Resume button from Home component

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -24,9 +24,6 @@ const Home = () => {
             <button
             className='bg-gradient-to-r from-green-400 to-blue-500 text-white
             transform transition-transform duration-300 hover:scale-105 px-4 py-2 rounded-full'>Contact With Me</button>
-            <button
-            className='bg-gradient-to-r from-pink-500 to-yellow-500 text-white
-            transform transition-transform duration-300 hover:scale-105 px-4 py-2 rounded-full'>Resume</button>
         </div>
 
     </div>

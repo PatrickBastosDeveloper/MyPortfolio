02d54d2333a8f8460fe2e3469ab21fe16dd22e80
PatrickBastosDeveloper <patrickbastosc@gmail.com>
2025-03-01 08:12:29 -0300

    refactor: Remove Service component and update App layout

--- src/App.jsx
+++ src/App.jsx
@@ -6,7 +6,6 @@ import Footer from './components/Footer'
 import Home from './components/Home'
 import Navbar from './components/Navbar'
 import Projects from './components/Projects'
-import Service from './components/Service'
 import ChatbotComponent from './components/ChatbotComponent';
 
 function App() {
@@ -17,7 +16,6 @@ function App() {
         <Home />
         <About />
         <ChatbotComponent />
-        <Service />
         <Projects />
         <Contact />
         <Footer />
